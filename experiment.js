c3d762d9a4e904e66aa0115378b039d89f90ff70
Muhammad Rizki Syauqani <rizki.syauqani04@gmail.com>
2022-06-26 16:12:44 +0700

    nais

--- experiment.js
+++ experiment.js
@@ -1,6 +1,14 @@
 const formElement = document.getElementById("inputBook");
 const submitButton = document.getElementById("bookSubmit");
 
+const dataBuku = {
+  id: +new Date(),
+  title: "",
+  author: "",
+  year: "",
+  isComplete: true,
+};
+
 function formHandling(event) {
   event.preventDefault();
 }
@@ -9,14 +17,18 @@ function submitFormData() {
   const judul = document.getElementById("inputBookTitle").value;
   const penulis = document.getElementById("inputBookAuthor").value;
   const tahun = document.getElementById("inputBookYear").value;
-  const kotakCentang = document.getElementById("inputBookIsComplete");
+  const kotakCentang = document.getElementById("inputBookIsComplete").checked;
+  dataBuku.title = judul;
+  dataBuku.author = penulis;
+  dataBuku.year = tahun;
+  dataBuku.isComplete = kotakCentang;
   if (kotakCentang.checked === false) {
     const belumSelesai = document.getElementById("belumSelesaiBaca");
     const nampungId = document.createElement("p");
     const nampungJudul = document.createElement("h3");
     const nampungPenulis = document.createElement("p");
     const nampungTahun = document.createElement("p");
-    nampungId.textContent = +new Date();
+    nampungId.textContent = dataBuku.id;
     nampungJudul.textContent = judul;
     nampungPenulis.textContent = penulis;
     nampungTahun.textContent = tahun;
