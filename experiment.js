52b44dd5d1b1e667407f2d797b308fb23ae2dc43
Muhammad Rizki Syauqani <rizki.syauqani04@gmail.com>
2022-06-26 19:26:49 +0700

    error making a pushing button

--- experiment.js
+++ experiment.js
@@ -1,12 +1,17 @@
 const formElement = document.getElementById("inputBook");
 const submitButton = document.getElementById("bookSubmit");
+const belumSelesai = document.getElementById("belumSelesaiBaca");
+const selesai = document.getElementById("SelesaiBaca");
+const nampungKeSelesai = "";
+
+const pushKeSelesai = [];
 
 const dataBuku = {
-  id: +new Date(),
+  id: "",
   title: "",
   author: "",
   year: "",
-  isComplete: true,
+  isComplete: "",
 };
 
 function formHandling(event) {
@@ -18,46 +23,44 @@ function submitFormData() {
   const penulis = document.getElementById("inputBookAuthor").value;
   const tahun = document.getElementById("inputBookYear").value;
   const kotakCentang = document.getElementById("inputBookIsComplete").checked;
+  dataBuku.id = +new Date();
   dataBuku.title = judul;
   dataBuku.author = penulis;
   dataBuku.year = tahun;
   dataBuku.isComplete = kotakCentang;
-  if (kotakCentang.checked === false) {
-    const belumSelesai = document.getElementById("belumSelesaiBaca");
+  if (kotakCentang === false) {
     const nampungId = document.createElement("p");
     const nampungJudul = document.createElement("h3");
     const nampungPenulis = document.createElement("p");
     const nampungTahun = document.createElement("p");
     nampungId.textContent = dataBuku.id;
-    nampungJudul.textContent = judul;
-    nampungPenulis.textContent = penulis;
-    nampungTahun.textContent = tahun;
-    // console.log(
-    //   nampungId,
-    //   nampungJudul,
-    //   nampungPenulis,
-    //   nampungTahun,
-    //   "belum selesai",
-    // );
+    nampungJudul.textContent = dataBuku.title;
+    nampungPenulis.textContent = dataBuku.author;
+    nampungTahun.textContent = dataBuku.year;
     belumSelesai.append(nampungId, nampungJudul, nampungPenulis, nampungTahun);
+    const tombolHijau = document.getElementById("bukuSelesaiBaca");
+    const tombolMerah = document.getElementById("hapusBukuBelumSelesaiBaca");
+    tombolHijau.addEventListener("click", function (target) {
+      const nampungkeSelesaiDiv = document.createElement("div");
+      nampungkeSelesaiDiv.append(pushKeSelesai);
+      target = findTodo(dataBuku.id);
+      if (target === null) return;
+
+      dataBuku.isComplete;
+      pushKeSelesai(nampungId, nampungJudul, nampungPenulis, nampungTahun);
+    });
   } else {
-    const selesai = document.getElementById("SelesaiBaca");
     const nampungId = document.createElement("p");
     const nampungJudul = document.createElement("h3");
     const nampungPenulis = document.createElement("p");
     const nampungTahun = document.createElement("p");
-    nampungId.textContent = +new Date();
-    nampungJudul.textContent = judul;
-    nampungPenulis.textContent = penulis;
-    nampungTahun.textContent = tahun;
-    // console.log(
-    //   nampungId,
-    //   nampungJudul,
-    //   nampungPenulis,
-    //   nampungTahun,
-    //   "selesai",
-    // );
+    nampungId.textContent = dataBuku.id;
+    nampungJudul.textContent = dataBuku.title;
+    nampungPenulis.textContent = dataBuku.author;
+    nampungTahun.textContent = dataBuku.year;
     selesai.append(nampungId, nampungJudul, nampungPenulis, nampungTahun);
+    const greenButton = document.getElementById("bukuBelumSelesaiBaca");
+    const redButton = document.getElementById("hapusBukuSelesaiBaca");
   }
 }
 formElement.addEventListener("submit", formHandling);
